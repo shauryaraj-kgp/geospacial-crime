91c47a6f1822a519bf85f4f4028c730d4eb153df
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-05-02 18:20:27 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import HomePage from './components/HomePage';
+import 'leaflet/dist/leaflet.css';
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
