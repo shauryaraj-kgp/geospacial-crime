b73d677b98f7ec3e16fcee57ec8a740bf1965cf4
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-05-26 17:15:18 +0530

    Updated DashBoard according to MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import DashBoard from './components/DashBoard';
 import MapPage from './components/MapPage';
 import 'leaflet/dist/leaflet.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path='/map' element={<MapPage />} />
-        <Route path='/' element={<DashBoard />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<DashBoard />} />
+          <Route path="map" element={<MapPage />} />
+        </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
+
   );
 }
 
