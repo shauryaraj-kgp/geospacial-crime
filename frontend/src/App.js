0278b7cbed31ddfd51d75259209cde83fa18fa8f
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-05-30 18:07:43 +0530

    Implemented Map-Libre

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import DashBoard from './components/DashBoard';
-import MapPage from './components/MapPage';
-import 'leaflet/dist/leaflet.css';
+import DisasterMap from './components/DisasterMap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
@@ -12,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<DashBoard />} />
-          <Route path="map" element={<MapPage />} />
+          <Route path="map" element={<DisasterMap />} />
         </Route>
       </Routes>
     </BrowserRouter>
