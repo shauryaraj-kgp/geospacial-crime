4ca85f71d0f02543e935b8246d6b4b6f5a7e597a
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-06-02 04:44:28 +0530

    Integrated with backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 export async function fetchHotspotSummary() {
-    const response = await fetch('http://localhost:8000/explain-hotspot/');
+    const response = await fetch('https://geospacial-crime-backend.onrender.com/explain-hotspot/');
     if (!response.ok) throw new Error('Failed to fetch hotspot summary');
     return await response.json();
 } 
