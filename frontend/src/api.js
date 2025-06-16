d4df443f5b749e75152786193740da3306419ce7
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-06-16 18:24:58 +0530

    railway deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://api-app-rn9y.onrender.com';
-const MODEL_URL = 'https://model-app-4bsd.onrender.com';
+const MODEL_URL = 'https://geospacial-crime-production.up.railway.app';
 // const API_URL = 'http://127.0.0.1:8000';
 // const MODEL_URL = 'http://127.0.0.1:10000';
 
